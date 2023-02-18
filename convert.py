d3a84ef7bf02e5ab083e6819a833212361ddd1fa
iamkish0re <kishoresreedhar1999@gmail.com>
2023-02-18 12:15:14 +0530

    Created Skeleton of converter

--- convert.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image 
-import PIL 
-  
-# creating a image object (main image) 
-im1 = Image.open(r"D:\Wallpaper\365604.jpg") 
-  
-# save a image using extension
-im1 = im1.save("geeks.jpg")
--- main.py
+++ main.py
@@ -1,18 +1,51 @@
+from pathlib import Path
+
 import PySimpleGUI as sg
+from PIL import Image
 
-sg.theme('DarkAmber')   # Add a touch of color
-# All the stuff inside your window.
-layout = [  [sg.Text('Some text on Row 1')],
-            [sg.Text('Enter something on Row 2'), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
+def is_path_valid(path):
+    if path and Path(path).exists():
+        True
+    sg.popup_error("Please check the File Paths!")
+    False
 
-# Create the Window
-window = sg.Window('Window Title', layout)
-# Event Loop to process "events" and get the "values" of the inputs
+def view_img(src_file):
+    image = sg.Image(src_file)
+    sg.popup_no_titlebar(image)
+
+def convert_png_to_jpg(src_file, trg_folder):
+    src_img = Image.open(src_file)
+    
+    file_name = Path(src_file).stem
+    output_file = Path(trg_folder) / f"{file_name}.jpg"
+    #The image object is used to save the image in jpg format
+    rgb = src.convert('RGB')
+    rgb.save(output_file)
+    sg.popup_no_titlebar("File Converted!")
+
+
+layout = [
+    # [sg.Image("C:/Users/kisho/Desktop/convert.jpg")],
+    [sg.Text("Source File     :"), sg.Input(key="-IN-"), sg.FileBrowse(file_types=(("Image Files", "*.*"),))],
+    [sg.Text("Target Folder  :"), sg.Input(key="-OUT-"), sg.FolderBrowse()],
+    [sg.Exit(), 
+    # sg.Button("View Image"), 
+    sg.Button("Convert Image")]
+]
+
+window = sg.Window("PixelPlay", layout)
+
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+    print(event, values)
+    if event in (sg.WINDOW_CLOSED, "Exit"):
         break
-    print('You entered ', values[0])
-
+    if event == "View Image":
+        if is_path_valid(values["-IN-"]):
+            view_img(values["-IN-"])
+    if event == "Convert Image":
+        if is_path_valid(values["-IN-"]) and is_path_valid(values["-OUT-"]):
+            convert_png_to_jpg(src=values["-IN-"], trg=values["-OUT-"])
+    
+    
 window.close()
